8c2bd93769e6b41bc33390fb63e279c7f9607db2
abin jose <abin111manjapra@gmail.com>
2017-11-10 02:34:00 +0530

    Basic controllers and models added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//licence
+Route::get('/licence', 'LoginController@licence')->name('licence');
+Route::get('/under-construction', 'LoginController@underConstruction')->name('under-construction');
+
+Route::group(['middleware' => 'is.guest'], function() {
+    Route::get('/', 'LoginController@publicHome')->name('public-home');
+    //user validity expired
+    Route::get('/user/expired', 'LoginController@userExpired')->name('user-expired');
+
+    Route::get('/login', 'LoginController@login')->name('login');
+    Route::post('/login/action', 'LoginController@loginAction')->name('login-action');
+});
